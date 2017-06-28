c4679eae981a9a32ca2e4750658f9c04a6a4f0ec
alisonailea <alisonstump@gmail.com>
2017-06-28 12:05:19 -0700

    basic transition animation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import Button from 'lucid-ui/Button';
 import bgImg from './1.jpg';
 import '../node_modules/lucid-ui/dist/index.css';
 import './Base.css';
-import './App.css';
+import './transitionAnimation.css';
+
+const cx = classNames;
 
 class App extends Component {
+  constructor(props) {
+		super(props);
+
+		this.state = {
+			isHovered: false,
+		};
+	}
+
+  setHover() {
+		this.setState({ isHovered: !this.state.isHovered });
+	}
+
   render() {
     return (
       <div className="App">
-        <img src={bgImg} className='Background-image' alt="an image of blured lines connecting buildings across a city"/>
+        <img
+          src={bgImg}
+          className='Background-image'
+          className={cx('Background-image', {
+						'Background-image-blur': this.state.isHovered,
+					})}
+          alt="an image of blured lines connecting buildings across a city"
+        />
         <div className='App-content'>
           <div className="App-header">
-            <h2>Animation 101</h2>
+            <h2
+              onMouseEnter={() => this.setHover()}
+              onMouseLeave={() => this.setHover()}
+            >
+              Animation 101
+            </h2>
           </div>
           <div className="App-animations">
             <Button className='Button'>Animate!</Button>
